13c284eeb928b3374540755b2168031898167774
emmabell42 <emmabell42@gmail.com>
2015-01-12 12:04:17 +0000

    Update SAMtoBAM.sh
    Edited loop to change location of samfiles to current directory.

--- SAMtoBAM.sh
+++ SAMtoBAM.sh
@@ -5,4 +5,4 @@ sed -i 's/.sam//g' samfiles
 while read LINE
 do
 /data/seqtools/samtools-1.1/samtools view -bS -o $LINE.bam $LINE.sam
-done < /data/emmabell42/Coverage_plots/BAM_files/samfiles
+done < ./samfiles
