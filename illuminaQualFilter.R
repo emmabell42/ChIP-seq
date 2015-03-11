75091abb9dd4002b0fd7ff9fe68601afa74c096e
emmabell42 <emmabell42@gmail.com>
2015-03-11 11:31:03 +0000

    Update illuminaQualFilter.R

--- illuminaQualFilter.R
+++ illuminaQualFilter.R
@@ -1,7 +1,11 @@
+## change this to what you expect/
+ideal.readlength <- 50
+
 ofile <- as.character(read.table("file_to_filter.txt")[[1]])
 setwd("Fastq")
 library(ShortRead)
 reads <- readFastq(ofile)
+ideal.readlength <- as.numeric(names(tail(sort(table(width(reads[1:100]))),n=1)))
 quals1 <- as(FastqQuality(quality(quality(reads[1:min(c(length(reads),20000000))]))),"matrix")
 if(length(reads)>20000000){
         quals2 <- as(FastqQuality(quality(quality(reads[c(20000001:length(reads))]))),"matrix")
@@ -11,10 +15,10 @@ if(length(reads)>20000000){
         trim2 <- apply(quals2,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<30))>5)))
         trim <- c(trim,trim2)
 }
-bad.q <- which(!is.infinite(trim) & trim<40)
+bad.q <- which(!is.infinite(trim) & trim<(0.8*ideal.readlength))
 reads <- reads[setdiff(c(1:length(reads)),bad.q)]
 trim <- trim[setdiff(c(1:length(reads)),bad.q)]
-trim[is.infinite(trim)] <- 50
+trim[is.infinite(trim)] <- ideal.readlength
 reads <- ShortReadQ(sread=subseq(sread(reads),start=rep(1,length(trim)),end=trim),quality=new(Class=class(quality(reads)),quality=subseq(quality(quality(read
 s)),start=rep(1,length(trim)),end=trim)),id=id(reads))
 no_n <- which(alphabetFrequency(sread(reads))[,'N']==0)
