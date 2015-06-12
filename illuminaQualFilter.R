0203198b674086775a432f3d7db0f862935d415c
emmabell42 <emmabell42@gmail.com>
2015-06-12 18:48:50 +0100

    Update illuminaQualFilter.R

--- illuminaQualFilter.R
+++ illuminaQualFilter.R
@@ -1,19 +1,24 @@
-## change this to what you expect/
-ideal.readlength <- 50
-
-ofile <- as.character(read.table("file_to_filter.txt")[[1]])
-setwd("Fastq")
+ofile <- as.character(read.table("tofilter")[[1]])
 library(ShortRead)
-reads <- readFastq(ofile)
+for(i in 14:length(ofile)){
+cat("Reading",ofile[i],as.character(Sys.time()),sep=" ",fill=T)
+reads <- readFastq(ofile[i])
 ideal.readlength <- as.numeric(names(tail(sort(table(width(reads[1:100]))),n=1)))
 quals1 <- as(FastqQuality(quality(quality(reads[1:min(c(length(reads),20000000))]))),"matrix")
 if(length(reads)>20000000){
-        quals2 <- as(FastqQuality(quality(quality(reads[c(20000001:length(reads))]))),"matrix")
+        quals2 <- as(FastqQuality(quality(quality(reads[c(20000001:40000000)]))),"matrix")
+if(length(reads)>40000000){
+        quals3 <- as(FastqQuality(quality(quality(reads[c(40000001:length(reads))]))),"matrix")
 }
 trim <- apply(quals1,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
 if(length(reads)>20000000){
         trim2 <- apply(quals2,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
-        trim <- c(trim,trim2)
+		trim <- c(trim,trim2)
+}
+if(length(reads)>40000000){
+        trim3 <- apply(quals3,MARGIN=1,function(x)min(which(cumsum(as.numeric(x<20))>5)))
+        trim <- c(trim,trim3)
+}
 }
 bad.q <- which(!is.infinite(trim) & trim<(0.8*ideal.readlength))
 good.reads <- setdiff(c(1:length(reads)),bad.q)
@@ -21,7 +26,9 @@ reads <- reads[good.reads]
 trim <- trim[good.reads]
 trim[is.infinite(trim)] <- ideal.readlength
 reads <- ShortReadQ(sread=subseq(sread(reads),start=rep(1,length(trim)),end=trim),quality=new(Class=class(quality(reads)),quality=subseq(quality(quality(reads)),start=rep(1,length(trim)),end=trim)),id=id(reads))
-no_n <- which(alphabetFrequency(sread(reads[1:100]))[,'N']==0)
-newfile <- strsplit(ofile,split=".fastq")[[1]][1]
+#no_n <- which(alphabetFrequency(sread(reads[1:100]))[,'N']==0)
+newfile <- strsplit(ofile[i],split=".fastq")[[1]][1]
 newfile <- paste(newfile,"_filtered.fq",sep="")
-writeFastq(reads[no_n],newfile)
+#writeFastq(reads[no_n],newfile)
+writeFastq(reads,newfile,compress=F)
+}
